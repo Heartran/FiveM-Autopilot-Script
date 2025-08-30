4ae622843f1935cc534559a37bfc0d261c753941
Heartran <51830223+Heartran@users.noreply.github.com>
2025-08-30 15:57:15 +0200

    Enable Lua 5.4 support in fxmanifest
    Added 'lua54 yes' to fxmanifest.lua to enable Lua 5.4 compatibility for the resource.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,7 @@ name 'autopilot'
 author 'Heartran'
 description 'Autopilota: registra veicolo personale e richiamalo ovunque'
 version '1.0.0'
+lua54 'yes'
 
 client_scripts {
     '@es_extended/imports.lua',
